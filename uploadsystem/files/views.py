9986f2c6693883026115c409b44b7a050c819892
Gurleen <gurleendhillonm1114@gmail.com>
2021-02-07 14:59:37 +0530

    limit on upload size

--- uploadsystem/files/views.py
+++ uploadsystem/files/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+from django.core.exceptions import ValidationError
 
 
 def upload(request):
     context = {}
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
+        if uploaded_file.size > 104857:
+            raise ValidationError("The maximum file size that can be uploaded is 10MB")
         fs=FileSystemStorage()
         name = fs.save(uploaded_file.name, uploaded_file)
         context['url'] = fs.url(name)
