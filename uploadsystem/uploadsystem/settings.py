e737540e912b8c08e55cd2299fbdb97f915c8194
Harmanjit Singh <harmanjit140500@gmail.com>
2021-02-10 11:14:02 +0530

    idk

--- uploadsystem/uploadsystem/settings.py
+++ uploadsystem/uploadsystem/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':BASE_DIR / 'db.sqlite3',
+        'NAME':'db.sqlite3',
     }
 }
 
